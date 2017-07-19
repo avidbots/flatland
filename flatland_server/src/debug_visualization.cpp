f882c8666654bbefe04f364605391854e4020bbc
Mike Brousseau <mike.brousseau@avidbots.com>
2017-07-19 10:34:04 -0400

    more clang fixes

--- flatland_server/src/debug_visualization.cpp
+++ flatland_server/src/debug_visualization.cpp
@@ -182,7 +182,7 @@ void DebugVisualization::Publish() {
     }
     topic.second.publisher.publish(topic.second.markers);
     topic.second.needs_publishing = false;
-    //ROS_INFO_THROTTLE_NAMED(1.0, "DebugVis", "Publishing %s",
+    // ROS_INFO_THROTTLE_NAMED(1.0, "DebugVis", "Publishing %s",
     //                        topic.first.c_str());
   }
 }
