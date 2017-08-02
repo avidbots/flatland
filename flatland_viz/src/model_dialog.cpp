44f678841c408d86d7c909bcbd6c43a4d579614c
Mike Brousseau <mike.brousseau.13@gmail.com>
2017-08-02 10:02:15 -0400

    work in progress

--- flatland_viz/src/model_dialog.cpp
+++ flatland_viz/src/model_dialog.cpp
@@ -53,6 +53,7 @@
 #include <QMessageBox>
 #include <QVBoxLayout>
 
+#include <flatland_server/timekeeper.h>
 #include <ros/ros.h>
 #include <boost/filesystem.hpp>
 #include <iostream>
@@ -194,6 +195,21 @@ void ModelDialog::SetButtonColor(const QColor *c, QPushButton *b) {
   color_button->setStyleSheet(qs);
 }
 
+void spawnModelClient() {
+  boost::filesystem::path this_file_dir;
+  boost::filesystem::path world_yaml;
+  boost::filesystem::path robot_yaml;
+  Timekeeper timekeeper;
+  ros::NodeHandle nh;
+  ros::ServiceClient client;
+  flatland_msgs::SpawnModel srv;
+  std::thread simulation_thread;
+  bool stop_thread;
+  World *w;
+
+  // std::string world_yaml =
+  //    this_file_dir / fs::path("load_world_tests/simple_test_A/world.yaml");
+}
 // // Register sigint shutdown handler
 // signal(SIGINT, SigintHandler);
 
@@ -212,3 +228,42 @@ void ModelDialog::SetButtonColor(const QColor *c, QPushButton *b) {
 //   ROS_INFO_STREAM_NAMED("Node", "Beginning ros shutdown");
 //   ros::shutdown();
 // }
+
+/*
+world_yaml =
+      this_file_dir / fs::path("load_world_tests/simple_test_A/world.yaml");
+
+  robot_yaml = this_file_dir /
+               fs::path("load_world_tests/simple_test_A/turtlebot.model.yaml");
+
+  w = World::MakeWorld(world_yaml.string());
+
+  srv.request.name = "service_manager_test_robot";
+  srv.request.ns = "robot123";
+  srv.request.yaml_path = robot_yaml.string();
+  srv.request.pose.x = 1;
+  srv.request.pose.y = 2;
+  srv.request.pose.theta = 3;
+
+  client = nh.serviceClient<flatland_msgs::SpawnModel>("spawn_model");
+
+  // Threading is required since client.call blocks executing until return
+  StartSimulationThread();
+
+  ASSERT_TRUE(client.call(srv));
+
+  StopSimulationThread();
+
+  ASSERT_TRUE(srv.response.success);
+  ASSERT_STREQ("", srv.response.message.c_str());
+
+  ASSERT_EQ(5, w->models_.size());
+  EXPECT_STREQ("service_manager_test_robot", w->models_[4]->name_.c_str());
+  EXPECT_STREQ("robot123", w->models_[4]->namespace_.c_str());
+  EXPECT_FLOAT_EQ(1, w->models_[4]->bodies_[0]->physics_body_->GetPosition().x);
+  EXPECT_FLOAT_EQ(2, w->models_[4]->bodies_[0]->physics_body_->GetPosition().y);
+  EXPECT_FLOAT_EQ(3, w->models_[4]->bodies_[0]->physics_body_->GetAngle());
+  EXPECT_EQ(5, w->models_[4]->bodies_.size());
+
+  delete w;
+*/
