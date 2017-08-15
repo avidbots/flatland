fe3a9098b266692fe27f1bde99c73d8af145b30c
Chunshang Li <chunshang.li@avidbots.com>
2017-08-15 11:12:21 -0400

    added doxygens script

---
+++ doxygen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dpkg -s doxygen
+
+# if doxygen is not installed, install doxygen
+if [ $? -ne 0 ]; then
+    echo "Doxygen not installed, installing doxygen"
+    sudo apt-get install doxygen -y
+fi
+
+# change directory to the doxygen file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# run doxygen
+doxygen Doxyfile
