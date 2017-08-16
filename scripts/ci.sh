339f79f2927744ffe3042d81036a5fca640fe3a9
Chunshang Li <chunshang.li@avidbots.com>
2017-08-16 15:54:42 -0400

    fix small bugs for ci scirpt

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-apt-get install sudo clang-format-3.8 clang-3.8 lcov -y 
+sudo apt-get install sudo clang-format-3.8 clang-3.8 lcov -y 
 
 # change to the file's directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd DIR/../
+cd $DIR/../
 
 # check files are correctly formatted
 git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -i && git diff --exit-code || { git reset --hard; false; } 
