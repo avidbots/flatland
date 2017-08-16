13e576d018fb780da6b334ad089d3622c9dd5146
Chunshang Li <chunshang.li@avidbots.com>
2017-08-16 16:22:59 -0400

    try the fix again

--- flatland_plugins/src/tricycle_drive.cpp
+++ flatland_plugins/src/tricycle_drive.cpp
@@ -44,8 +44,8 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <Box2D/Box2D.h>
 #include <flatland_plugins/tricycle_drive.h>
-#include <flatland_server/Box2D/Box2D.h>
 #include <flatland_server/debug_visualization.h>
 #include <flatland_server/model_plugin.h>
 #include <flatland_server/yaml_reader.h>
