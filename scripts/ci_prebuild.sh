4e1caa28c8393dd81671daef164cd8443a18be15
Chunshang Li <chunshang.li@avidbots.com>
2017-08-16 17:07:11 -0400

    first build with static analyzer under install then run test with no install

--- scripts/ci_prebuild.sh
+++ scripts/ci_prebuild.sh
@@ -8,11 +8,3 @@ cd $DIR/../
 
 # check files are correctly formatted
 git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -i && git diff --exit-code || { git reset --hard; false; } 
-
-# cd into catkin workspace
-cd ~/catkin_ws
-catkin clean -y
-catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_server
-run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_plugins
-run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_viz
