c00cdf9ef2d5045c6ae10ee4c423e084ecbaeed3
Chunshang Li <chunshang.li@avidbots.com>
2017-08-16 17:33:50 -0400

    made the static analyzer run after testsing

--- scripts/ci_postbuild.sh
+++ scripts/ci_postbuild.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
 # cd into catkin workspace
-# catkin clean -y
-# catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-
 cd /root/catkin_ws
+catkin config --install
+catkin clean -y
+catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_server
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_plugins
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_viz
