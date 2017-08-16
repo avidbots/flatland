913923cabaae8e040a8ef75efb5d72a46f7c25ba
Chunshang Li <chunshang.li@avidbots.com>
2017-08-16 17:49:16 -0400

    added post build option

--- scripts/ci_postbuild.sh
+++ scripts/ci_postbuild.sh
@@ -9,3 +9,5 @@ catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_server
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_plugins
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_viz
+
+echo "ci_postbuild.sh completed."
--- scripts/ci_prebuild.sh
+++ scripts/ci_prebuild.sh
@@ -8,3 +8,5 @@ cd $DIR/../
 
 # check files are correctly formatted
 git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -i && git diff --exit-code || { git reset --hard; false; } 
+
+echo "ci_prebuild.sh completed."
