d95fa4023c0aec5e8363f4d0308a713ef6d594f8
Chunshang Li <chunshang.li@avidbots.com>
2017-08-16 17:27:24 -0400

    removed useless lines

--- scripts/ci_prebuild.sh
+++ scripts/ci_prebuild.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
 
 # check files are correctly formatted
-git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -i && git diff --exit-code || { git reset --hard; false; } 
+# git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -i && git diff --exit-code || { git reset --hard; false; } 
 
 # cd into catkin workspace
 cd /root/catkin_ws
@@ -15,7 +15,6 @@ catkin config --install
 catkin clean -y
 catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-cd /root/catkin_ws
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_server
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_plugins
 run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_viz
