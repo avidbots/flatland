148832304e38cb72ea8e9e0d2c9ae17ec7f05ad9
Chunshang Li <chunshang.li@avidbots.com>
2017-08-17 14:49:09 -0400

    reverted back to run tests first

--- scripts/ci_postbuild.sh
+++ scripts/ci_postbuild.sh
@@ -1,20 +1,13 @@
 #!/bin/bash -e
 
-sudo apt-get install sudo clang-tidy-3.8 -y 
-
 # cd into catkin workspace
 cd /root/catkin_ws
 
-# run static analyzer on the first build in the install space
-# -DCMAKE_EXPORT_COMPILE_COMMANDS=ON is set in travis.yml
-run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_server
-run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_plugins
-run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_viz
-
-# run tests
-catkin config --no-install
+# run static analyzer
 catkin clean -y
-catkin build
-catkin build --catkin-make-args run_tests
+catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_server | /root/catkin_ws/src/flatland/scripts/parse_clang_tidy.py
+run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_plugins | /root/catkin_ws/src/flatland/scripts/parse_clang_tidy.py
+run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_viz | /root/catkin_ws/src/flatland/scripts/parse_clang_tidy.py
 
 echo "ci_postbuild.sh completed."
