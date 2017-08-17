266d5f1fffe8f6f8dfcffd5f26847d05c2552421
Chunshang Li <chunshang.li@avidbots.com>
2017-08-17 15:11:50 -0400

    added echo statements

--- scripts/ci_prebuild.sh
+++ scripts/ci_prebuild.sh
@@ -7,6 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
 
 # check files are correctly formatted
+
+echo "running clang format..."
 # git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -i && git diff --exit-code || { git reset --hard; false; } 
 
 echo "ci_prebuild.sh completed."
