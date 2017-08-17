a821057e44919cc97de169e09b4155297915ddec
Chunshang Li <chunshang.li@avidbots.com>
2017-08-17 11:55:09 -0400

    manually run tests in post ci script

--- scripts/ci_postbuild.sh
+++ scripts/ci_postbuild.sh
@@ -2,12 +2,17 @@
 
 # cd into catkin workspace
 cd /root/catkin_ws
-catkin config --install
-catkin clean -y
-catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_server
-run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_plugins
-run-clang-tidy-3.8.py -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_viz
+# run static analyzer on the first build in the install space
+# -DCMAKE_EXPORT_COMPILE_COMMANDS=ON is set in travis.yml
+run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_server
+run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_plugins
+run-clang-tidy-3.8.py -j1 -clang-tidy-binary=clang-tidy-3.8 -p=build/flatland_viz
+
+# run tests
+catkin config --no-install
+catkin clean -y
+catkin build
+catkin build --catkin-make-args run_tests
 
 echo "ci_postbuild.sh completed."
