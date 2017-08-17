aad39bb4138a2fc3bf8c801a5b877db34a1bfbbb
Chunshang Li <chunshang.li@avidbots.com>
2017-08-17 16:20:54 -0400

    fixed bug in pre built script

--- scripts/ci_prebuild.sh
+++ scripts/ci_prebuild.sh
@@ -16,9 +16,9 @@ cd $DIR/../
 # check files are correctly formatted
 
 echo "running clang format..."
-git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -output-replacements-xml | grep -c "<replacement "
+CLANG_CHANGES_CNT=$(git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -output-replacements-xml | grep -c "<replacement " || true)
 
-if [ $? -ne 1 ]
+if [ $CLANG_CHANGES_CNT -ne 0 ]
 then 
     printf "\n\n"
 
@@ -30,7 +30,7 @@ then
     print_er "the files processed by clang-format need following replacements"
     git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -output-replacements-xml
 
-    printf "\n^^^^ See above for clang format output ^^^^\n"    
+    printf "\n^^^^ See above for clang format output, each <replacements> corresponds to one file ^^^^\n"    
 
     print_er "Clang Format Error!"
     echo 'File not formatted correctly, please execute the command below in flatland repo to see what needs to be changed'
