58371f5b7934bb0a51fb89448323540b5e1a586b
Chunshang Li <chunshang.li@avidbots.com>
2017-08-22 12:02:57 -0400

    changed conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -168,4 +168,10 @@ texinfo_documents = [
 ]
 
 
+# for doxygen
+import subprocess, os
 
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+    subprocess.call('cd ../; ./doxygen.sh', shell=True)
