3175f969e1a6c0542236703f90634a3c212bdc8d
Chunshang Li <chunshang.li@avidbots.com>
2017-08-22 12:06:17 -0400

    going to do something differently

--- docs/conf.py
+++ docs/conf.py
@@ -168,10 +168,4 @@ texinfo_documents = [
 ]
 
 
-# for doxygen
-import subprocess, os
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-if read_the_docs_build:
-    subprocess.call('cd ../; ./doxygen.sh', shell=True)
