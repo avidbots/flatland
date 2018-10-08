8b674445610bca36e36ed87485933a5e57d9420a
Joseph Duchesne <joseph@avidbots.com>
2018-10-08 01:08:21 -0400

    cleaned up changes

--- flatland_viz/src/spawn_model_tool.cpp
+++ flatland_viz/src/spawn_model_tool.cpp
@@ -110,7 +110,7 @@ void SpawnModelTool::onInitialize() {
   model_state = m_hidden;
 
   // make an arrow to show axis of rotation
-  arrow_ = new rviz::Arrow(scene_manager_, NULL, 2.0f, 0.2f, 0.5f, 0.35f);
+  arrow_ = new rviz::Arrow(scene_manager_, NULL, 2.0f, 0.2f, 0.3f, 0.35f);
   arrow_->setColor(0.0f, 0.0f, 1.0f, 1.0f);  // blue
   arrow_->getSceneNode()->setVisible(
       false);  // will only be visible during rotation phase
@@ -118,16 +118,6 @@ void SpawnModelTool::onInitialize() {
   // set arrow to point up (along z)
   Ogre::Quaternion orientation(Ogre::Radian(M_PI), Ogre::Vector3(1, 0, 0));
   arrow_->setOrientation(orientation);
-
-  // // full path to the model file
-  // model_resource_ = "package://flatland_viz/media/simple.dae";
-
-  // // load the 3d model
-  // if (rviz::loadMeshFromResource(model_resource_).isNull()) {
-  //   ROS_ERROR("SpawnModelTool: failed to load model resource '%s'.",
-  //             model_resource_.c_str());
-  //   return;
-  // }
 
   // create an Ogre child scene node
   moving_model_node_ =
