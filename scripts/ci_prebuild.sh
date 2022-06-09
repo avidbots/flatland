543d83d6a2202219e27772b26e6aee6ee3a1b8c9
Joseph Duchesne <joseph@avidbots.com>
2022-06-09 15:22:25 -0400

    switched CI to try ros2 foxy build

--- scripts/ci_prebuild.sh
+++ scripts/ci_prebuild.sh
@@ -16,25 +16,25 @@ cd $DIR/../
 # check files are correctly formatted
 
 echo "running clang format..."
-CLANG_CHANGES_CNT=$(git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -output-replacements-xml | grep -c "<replacement " || true)
+CLANG_CHANGES_CNT=$(git ls-files | grep -E '\.[ch](pp)?$' | grep -v "flatland_box2d/" | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -output-replacements-xml | grep -c "<replacement " || true)
 
 if [ $CLANG_CHANGES_CNT -ne 0 ]
 then 
     printf "\n\n"
 
     print_er "clang-format was operated on following files:"
-    git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" 
+    git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" | grep -v "flatland_box2d/"
 
     printf "\n"
 
     print_er "the files processed by clang-format need following replacements"
-    git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -output-replacements-xml
+    git ls-files | grep -E '\.[ch](pp)?$' | grep -v "thirdparty/" | grep -v "flatland_box2d/" |  xargs clang-format-3.8 --style=file -output-replacements-xml
 
     printf "\n^^^^ See above for clang format output, each <replacements> corresponds to one file ^^^^\n"    
 
     print_er "Clang Format Error!"
     echo 'File not formatted correctly, please execute the command below in flatland repo to see what needs to be changed'
-    echo 'git ls-files | grep -E "\.[ch](pp)?$" | grep -v "thirdparty/" |  xargs clang-format-3.8 --style=file -i && git diff --exit-code'
+    echo 'git ls-files | grep -E "\.[ch](pp)?$" | grep -v "thirdparty/" | grep -v "flatland_box2d/" |  xargs clang-format-3.8 --style=file -i && git diff --exit-code'
     exit 1;
 fi
 
