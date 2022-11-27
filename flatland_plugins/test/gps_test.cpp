4191274bab1ee93f8c2c1d5f872f876236198d2c
Ana Barros <up201806593@fe.up.pt>
2022-11-27 22:49:57 +0000

    Fix gps test

--- flatland_plugins/test/gps_test.cpp
+++ flatland_plugins/test/gps_test.cpp
@@ -1,16 +1,17 @@
-#include <flatland_plugins/gps.h>
 #include <flatland_server/model_plugin.h>
 #include <gtest/gtest.h>
-#include <pluginlib/class_loader.h>
+#include <pluginlib/class_loader.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 TEST(GpsPluginTest, load_test) {
+  std::shared_ptr<rclcpp::Node> node =
+      rclcpp::Node::make_shared("test_gps_plugin");
   pluginlib::ClassLoader<flatland_server::ModelPlugin> loader(
       "flatland_server", "flatland_server::ModelPlugin");
 
   try {
-    boost::shared_ptr<flatland_server::ModelPlugin> plugin =
-        loader.createInstance("flatland_plugins::Gps");
+    std::shared_ptr<flatland_server::ModelPlugin> plugin =
+        loader.createSharedInstance("flatland_plugins::Gps");
   } catch (pluginlib::PluginlibException& e) {
     FAIL() << "Failed to load GPS plugin. " << e.what();
   }
