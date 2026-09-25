d78c9e62d20d9466453eec2375970767306ce10b
Joseph Duchesne <joseph@upsiderobotics.com>
2026-09-25 17:45:54 +0000

    Fixed dev container init

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-setup_line='source /opt/overlay_ws/install/setup.bash'
+setup_line='if [ -f /opt/overlay_ws/install/setup.bash ]; then source /opt/overlay_ws/install/setup.bash; else for setup_file in /opt/ros/*/setup.bash; do if [ -f "$setup_file" ]; then source "$setup_file"; break; fi; done; fi'
+if grep -Fxq 'source /opt/overlay_ws/install/setup.bash' "$HOME/.bashrc"; then
+    sed -i '\|^source /opt/overlay_ws/install/setup.bash$|d' "$HOME/.bashrc"
+fi
 if ! grep -Fxq "$setup_line" "$HOME/.bashrc"; then
     printf '\n%s\n' "$setup_line" >> "$HOME/.bashrc"
 fi
